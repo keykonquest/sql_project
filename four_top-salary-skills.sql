afef1ee957d6f92709feb7940afcaf99e0d9812e
Key <kpillorsrocks@gmail.com>
2024-07-09 15:35:00 -0400

    Complete fourth query challenge

--- four_top-salary-skills.sql
+++ four_top-salary-skills.sql
@@ -6,3 +6,15 @@ Answer: What are the top skills based on salary?
     helps identify the most financially rewarding skills to acquire or improve
 */
 
+SELECT
+    skills,
+    ROUND(AVG(salary_year_avg), 2) AS salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE 
+    job_title_short = 'Data Analyst' AND
+    salary_year_avg IS NOT NULL
+GROUP BY skills
+ORDER BY salary DESC
+LIMIT 10

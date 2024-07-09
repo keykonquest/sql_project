e49b8b147e67080dd92fc5f975f67f117714f522
Key <kpillorsrocks@gmail.com>
2024-07-09 16:54:07 -0400

    Complete fifth query challenge

--- five_optimal-skills.sql
+++ five_optimal-skills.sql
@@ -5,3 +5,39 @@ Answer: What are the most optimal skills to learn (aka it’s in high demand and
 - Why? Targets skills that offer job security (high demand) and financial benefits (high salaries), 
     offering strategic insights for career development in data analysis
 */
+
+
+WITH skill_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_level
+    FROM 
+        job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE 
+        job_title_short = 'Data Analyst' AND
+        salary_year_avg IS NOT NULL
+    GROUP BY skills_dim.skill_id
+), salary_avg AS (
+    SELECT
+        skills_job_dim.skill_id,
+        ROUND(AVG(salary_year_avg), 2) AS salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE 
+        job_title_short = 'Data Analyst' AND
+        salary_year_avg IS NOT NULL
+    GROUP BY skills_job_dim.skill_id
+)
+
+SELECT
+    skill_demand.skill_id,
+    skill_demand.skills,
+    demand_level,
+    salary
+FROM
+    skill_demand
+INNER JOIN salary_avg ON skill_demand.skill_id = salary_avg.skill_id
